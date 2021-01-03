e8582af86c54182e117a065417931e90fe682e96
Harsh8279 <pharsh.spt@gmail.com>
2021-01-03 16:07:49 +0530

    add all files first time

---
+++ src/test/java/DailyCodeBuffer/JWT/JwtdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package DailyCodeBuffer.JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
